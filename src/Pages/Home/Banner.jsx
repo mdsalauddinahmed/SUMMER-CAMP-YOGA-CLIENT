5bf75750a85f4df6749729c765327314045fa0dd
SalauddinAmir <mdsalauddinahmed.amir@gmail.com>
2023-06-12 00:57:09 +0600

    banner text added

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -11,10 +11,10 @@ import "swiper/css/pagination";
 
 // import required modules
 import { Navigation, Pagination } from "swiper";
- import banner4 from '../../assets/cover4.jpg'
- import banner2 from '../../assets/cover2.jpg'
+ import banner4 from '../../assets/image1.avif'
+ import banner2 from '../../assets/tvm4.webp'
  import banner1 from '../../assets/cover3.jpg'
- import banner5 from '../../assets/banner.jpg'
+ import banner5 from '../../assets/img2.jpeg'
  
 const Banner = () => {
      
@@ -37,18 +37,20 @@ const Banner = () => {
       >
         <SwiperSlide style={slideStyle}  >
              <div className="h-[600px] ">
-              <div>
-              <h1>hello world</h1>
+             <div className="text-white opacity-50 p-8   bg-cyan-900  max-w-7xl mx-24 mt-28">
+              <h1 className="text-4xl font-bold">"Discover the Art of Mindful Movement: Yoga for Body and Soul"</h1>
+              <p className="my-10">We believe that yoga is not just a physical practice but a path to self-discovery and personal growth. It offers a sanctuary where you can quiet the noise of the outside world, tune into your inner wisdom, and embrace a sense of tranquility and mindfulness. Through regular practice, you will witness the transformative power of yoga unfold in your life, promoting a greater sense of well-being, clarity of mind, and harmony in all aspects of your existence.</p>
+              <button className="btn bg-cyan-950 text-white ">Join Us</button>
               </div>
              </div>
           
            </SwiperSlide>
         <SwiperSlide style={{"background-image":`url(${banner2})`,"background-size":'cover'}} >
              <div className="h-[600px] ">
-              <div className="text-white   max-w-7xl mx-24 mt-28">
-              <h1>hello world</h1>
-              <p className="my-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione <br /> in rerum nulla temporibus aliquid, unde nihil, voluptatibus quidem qui maiores aut. <br /> Blanditiis iste voluptatum ipsa reprehenderit. Eligendi nesciunt maiores sint tenetur <br /> adipisci doloribus dolores ipsam soluta reiciendis iste amet quaerat pariatur impedit <br /> nam vero dignissimos, consectetur recusandae aliquam voluptatum. Est.</p>
-              <button className="btn ">Join Us</button>
+              <div className="text-white p-8  opacity-50  bg-cyan-900   max-w-7xl mx-24 mt-28">
+              <h1 className="text-4xl font-bold">"Transform Your Life Through Yoga: Embrace the Journey"</h1>
+              <p className="my-10">Through the art of mindful movement, you will learn to synchronize your breath with gentle and invigorating yoga poses, cultivating balance, strength, and flexibility. Our classes are designed to nourish both your body and soul, allowing you to release tension, reduce stress, and awaken your inner radiance.</p>
+              <button className="btn bg-cyan-950 text-white ">Join Us</button>
               </div>
             
              </div>
@@ -56,16 +58,20 @@ const Banner = () => {
            </SwiperSlide>
         <SwiperSlide  style={{"background-image":`url(${banner5})`,"background-size":'cover'}} >
              <div className="h-[600px] ">
-              <div>
-              <h1>hello world</h1>
+             <div className="text-white p-8  opacity-50  bg-cyan-900   max-w-7xl mx-24 mt-28">
+              <h1 className="text-4xl font-bold">"Cultivate Peace and Tranquility: Begin Your Yoga Practice Today"</h1>
+              <p className="my-10">We believe that yoga is not just a physical practice but a path to self-discovery and personal growth. It offers a sanctuary where you can quiet the noise of the outside world, tune into your inner wisdom, and embrace a sense of tranquility and mindfulness. Through regular practice, you will witness the transformative power of yoga unfold in your life, promoting a greater sense of well-being, clarity of mind, and harmony in all aspects of your existence.</p>
+              <button className="btn bg-cyan-950 text-white ">Join Us</button>
               </div>
              </div>
           
            </SwiperSlide>
         <SwiperSlide style={{"background-image":`url(${banner4})`,"background-size":'cover'}}  >
              <div className="h-[600px] ">
-              <div>
-              <h1>hello world</h1>
+             <div className="text-white p-8  opacity-50  bg-cyan-900   max-w-7xl mx-24 mt-28">
+              <h1 className="text-4xl font-bold">"Experience Blissful Harmony: Unlock the Benefits of Yoga"</h1>
+              <p className="my-10">Through the art of mindful movement, you will learn to synchronize your breath with gentle and invigorating yoga poses, cultivating balance, strength, and flexibility. Our classes are designed to nourish both your body and soul, allowing you to release tension, reduce stress, and awaken your inner radiance.</p>
+              <button className="btn bg-cyan-950 text-white ">Join Us</button>
               </div>
              </div>
           
